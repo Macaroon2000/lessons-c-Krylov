e4d9378bb45c3ad12728813c363d6e1de1e8b2e4
Macaroon <snis@yandex.ru>
2022-08-09 22:49:32 +0300

    commit

--- cringe/cringe/Program.cs
+++ cringe/cringe/Program.cs
@@ -9,23 +9,52 @@ namespace cringe
     internal class Program
     {
 
-           
-            static void Main(string[] args)
-            {
-                // задание 1
-                Console.WriteLine("Расчет индекса массы политоты");
 
-                Console.WriteLine("Введите ваше имя:");
-                string name = Console.ReadLine();
-                Console.WriteLine("Введите ваш уровень удовлетворения жизнью:");
-                byte lsl = Convert.ToInt8(Console.ReadLine());
-            Console.WriteLine("Введите ваш возраст:");
-                int age = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Введите ваш рост:");
-                double height = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Введите ваш вес:");
-                double weight = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine($"={name}\nФИО:\t{lsl}\nЛет:\t{age}\nРост:\t{height}\nВес:\t{weight}");
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Расчет индекса массы политоты");
+
+            Console.WriteLine("Введите ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите ваш уровень удовлетворения жизнью от 1 до 10:");
+            int opinion0 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ваш уровень удовлетворения людьми с именем Володя от 1 до 10:");
+            int opinion1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ваш уровень удовлетворения городом вашего проживания от 1 до 10:");
+            int opinion2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ваш уровень удовлетворения вашим финансовым положением от 1 до 10:");
+            int opinion3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ваш уровень удовлетворения в личной жизни от 1 до 10:");
+            int opinion4 = Convert.ToInt32(Console.ReadLine());
+
+            double I = (opinion0 + opinion1 + opinion2 + opinion3 + opinion4);
+
+            {
+
+
+                if (I > 40)
+                {
+                    Console.WriteLine($"{name} Аполитичен");
+
+                }
+                else if (I > 20)
+                {
+                    Console.WriteLine($"{name} Взволнован");
+
+
+                }
+                else if (I < 20)
+                    Console.WriteLine($"{name} Виновен");
+
+                {
+                    Console.ReadLine();
+
+
+                }
+
             }
-    }
-}
+        }
+
+    
+
